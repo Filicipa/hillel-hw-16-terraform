9988852b52db4745f33cecb340de7676862399b5
Filin <filicipa@gmail.com>
2023-01-22 21:48:45 +0200

    Create instance

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-03e08697c325f02ab"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
